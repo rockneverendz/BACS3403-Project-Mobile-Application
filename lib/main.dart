c77dc56f9ab48866f2fea6d79b48b024cf8d9793
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-10-29 18:59:42 +0800

    Update main.dart
    https://stackoverflow.com/a/61312927/13169165

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bacs3403_project_app/insertToken.dart';
 import 'package:bacs3403_project_app/tokenVerified.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +7,12 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
-  DotEnv().load('.env');
+  DotEnv().load('assets/env/.env');
+
+  // Overcome CERTIFICATE_VERIFY_FAILED
+  // Not recommended, but solves the problem.
+  HttpOverrides.global = new MyHttpOverrides();
+
   runApp(MyApp());
 }
 
@@ -23,5 +30,13 @@ class MyApp extends StatelessWidget {
         //'/about': (context) => TokenVerified()
       },
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
