447e3669fd9e37fe76183ead168afdc681e82025
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-11-01 18:13:32 +0800

    install camera libraries

---
+++ lib/verifyFace.dart
@@ -0,0 +1,14 @@
+import 'package:bacs3403_project_app/model/candidate.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class VerifyFace extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Get candidate information from main
+    Candidate candidate = Candidate.of(context);
+
+    return Scaffold(body: Container());
+  }
+}
