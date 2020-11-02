32771086619cd96965bb1d9dd809114f12dc97f0
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-11-03 02:58:28 +0800

    Add Face Recognition module

--- lib/verifyFace.dart
+++ lib/verifyFace.dart
@@ -1,14 +1,139 @@
-import 'package:bacs3403_project_app/model/candidate.dart';
+import 'dart:async';
+
+import 'package:bacs3403_project_app/viewFace.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class VerifyFace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Get candidate information from main
-    Candidate candidate = Candidate.of(context);
+    return Scaffold(
+      body: FutureBuilder<CameraDescription>(
+        future: intiCamera(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return TakePictureScreen(
+              camera: snapshot.data,
+            );
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
 
-    return Scaffold(body: Container());
+  Future<CameraDescription> intiCamera() async {
+    // Ensure that plugin services are initialized so that `availableCameras()`
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Obtain a list of the available cameras on the device.
+    final List<CameraDescription> cameras = await availableCameras();
+
+    // Get a specific camera from the list of available cameras.
+    return cameras.first;
+  }
+}
+
+// A screen that takes in a list of cameras and the Directory to store images.
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  // Add two variables to the state class to store the CameraController and
+  // the Future.
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.veryHigh,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner
+      // until the controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the
+            // pattern package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
   }
 }
---
+++ lib/viewFace.dart
@@ -0,0 +1,164 @@
+// A widget that displays the picture taken by the user.
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+
+import 'model/candidate.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text("Is this a clear image of you?"),
+              ),
+              SizedBox(width: 250, child: Image.file(File(imagePath))),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: FlatButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text("Retake"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: SubmitButton(file: File(imagePath)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SubmitButton extends StatefulWidget {
+  final File file;
+
+  const SubmitButton({Key key, this.file}) : super(key: key);
+
+  @override
+  _SubmitButtonState createState() => _SubmitButtonState(file);
+}
+
+class _SubmitButtonState extends State<SubmitButton> {
+  final File file;
+  bool isLoading = false;
+
+  _SubmitButtonState(this.file);
+
+  @override
+  Widget build(BuildContext context) {
+    // Get candidate information from main
+    Candidate candidate = Candidate.of(context);
+
+    return ElevatedButton(
+      onPressed: () => handleSubmit(candidate.token, file),
+      child: (isLoading) ? CircularProgressIndicator() : Text("Continue"),
+    );
+  }
+
+  handleSubmit(String token, File file) {
+    setLoading(true);
+
+    submitPhoto(token, file).then((candidate) {
+      setLoading(false);
+      Navigator.push(
+        this.context,
+        // TODO next page
+        MaterialPageRoute(builder: (context) => null),
+      );
+    }, onError: (error) {
+      setLoading(false);
+      Scaffold.of(this.context).showSnackBar(
+        SnackBar(content: Text(error.toString())),
+      );
+    });
+  }
+
+  void setLoading(bool bool) {
+    setState(() {
+      isLoading = bool;
+    });
+  }
+
+  Future<Candidate> submitPhoto(String token, File imageFile) async {
+    // Prepare image to upload
+    // TODO called on null
+    final stream = new http.ByteStream(imageFile.openRead());
+    final length = await imageFile.length();
+
+    // Prepare post uri
+    final authority = DotEnv().env['API_URL'];
+    final path = '/api/Candidates/VerifyCandidate';
+    final param = {'token': token};
+    final uri = Uri.https(authority, path, param);
+
+    var request = new http.MultipartRequest("POST", uri);
+    var multipartFile = new http.MultipartFile('face', stream, length,
+        filename: "face" + extension(imageFile.path));
+
+    // Add token and image to request
+    request
+      ..fields['token'] = token
+      ..files.add(multipartFile);
+
+    try {
+      var streamedResponse =
+          await request.send().timeout(Duration(seconds: 10));
+
+      // Success
+      if (streamedResponse.statusCode == HttpStatus.ok) {
+        return Future.error('Ok! Valid!');
+      }
+      // Not Found
+      else if (streamedResponse.statusCode == HttpStatus.notFound) {
+        return Future.error('Not found!');
+      }
+      // Not Found
+      else if (streamedResponse.statusCode == HttpStatus.badRequest) {
+        http.Response response = await http.Response.fromStream(streamedResponse);
+        Map<String, dynamic> json = jsonDecode(response.body);
+        return Future.error(json['Message']);
+      }
+      // Other
+      else {
+        return Future.error(
+          'Error ${streamedResponse.statusCode.toString()} ${streamedResponse.reasonPhrase}',
+        );
+      }
+    } on SocketException {
+      return Future.error('SocketException : Failed to establish connection');
+    } on TimeoutException {
+      return Future.error('TimeoutException : Failed to establish connection');
+    } on Exception catch (Exception) {
+      print(Exception);
+      return Future.error(Exception.runtimeType.toString());
+    }
+  }
+}
